a9cdf063d5dfc45d850c648402d9bee4a7c42dbb
shruthishankar <shruthi15shankar@gmail.com>
2017-04-27 22:28:37 +0530

    First JS assignment

---
+++ app.js
@@ -0,0 +1,11 @@
+var Name ="Shruthi";
+console.log("Name : " + Name);
+
+var Age ="20";
+console.log("Age : " + Age);
+
+var DOB = "15-12-1997";
+console.log("Date Of Birth : " + DOB);
+
+var place = "Shimoga";
+console.log("Place Of Birth : " + place);
